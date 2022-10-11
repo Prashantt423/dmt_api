492f65fe02234c46b0d6c5063d5e1f7b1ff01d25
Prashantt423 <84055497+Prashantt423@users.noreply.github.com>
2022-10-11 20:14:56 +0530

    create product added

--- controller/product.routes.ts
+++ controller/product.routes.ts
@@ -1,11 +1,14 @@
 import {  Request, Response,NextFunction } from 'express';
+import { ProductUseCase } from '../usecase/product/product.usecase';
 const express = require('express');
 const router = express.Router();
 
 
-router.get("/read", (req:Request, res:Response, next:NextFunction):void => {
+router.post("/create",async (req:Request, res:Response, next:NextFunction):Promise<any> => {
   try {
-    res.send({ message: 'Hi there' });
+    const productObj =  await ProductUseCase.createProduct(req.body);
+    // console.log(productObj)
+    res.status(productObj.status).json(productObj);
   } catch (e) {
     next(e);
   }
