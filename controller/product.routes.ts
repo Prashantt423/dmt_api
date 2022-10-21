49fdfe88da38e4964ea56f998a399e6600522d90
Prashantt423 <84055497+Prashantt423@users.noreply.github.com>
2022-10-21 12:02:41 +0530

    pagination feature

--- controller/product.routes.ts
+++ controller/product.routes.ts
@@ -1,17 +1,47 @@
-import {  Request, Response,NextFunction } from 'express';
+import { Request, Response, NextFunction, RequestHandler } from 'express';
 import { ProductUseCase } from '../usecase/product/product.usecase';
+import { ProductService } from '../service/product.service';
+
 const express = require('express');
 const router = express.Router();
 
-
-router.post("/create",async (req:Request, res:Response, next:NextFunction):Promise<any> => {
-  try {
-    const productObj =  await ProductUseCase.createProduct(req.body);
-    // console.log(productObj)
-    res.status(productObj.status).json(productObj);
-  } catch (e) {
-    next(e);
+router.post(
+  '/create',
+  async (req: Request, res: Response, next: NextFunction): Promise<any> => {
+    try {
+      const productObj = await ProductService.createProduct(req.body);
+      res.status(productObj.status).json(productObj);
+    } catch (e) {
+      next(e);
+    }
   }
-});
+);
+
+router.delete(
+  '/delete',
+  async (req: Request, res: Response, next: NextFunction): Promise<any> => {
+    try {
+      const deletedProductObj = await ProductService.deleteProduct(req.body.id);
+
+      res.status(deletedProductObj.status).json(deletedProductObj);
+    } catch (e) {
+      next(e);
+    }
+  }
+);
+
+// /product?isFirst=1&limit=n -> next page and data
+router.get(
+  '/',
+  async (req: Request, res: Response, next: NextFunction): Promise<any> => {
+    try {
+      const foundProducts = await ProductUseCase.getProduct(req.query);
+      res.status(foundProducts.status).json(foundProducts);
+    } catch (e) {
+      console.log(e);
+      next(e);
+    }
+  }
+);
 
 module.exports = router;
--- model/product.model.ts
+++ model/product.model.ts
@@ -1,69 +1,74 @@
 import mongoose, { Model, Schema } from "mongoose";
-import { ProductType ,DiscountType,AttributeType,VariantType,ProductDetailsType} from "../types/product.types";
+import { ProductType, DiscountType, AttributeType, VariantType, ProductDetailsType } from "../types/product.types";
 const discountSchema = new Schema<DiscountType>({
-    value:{
-        type:Number,
-        min:0.01,
-        max:100
-    },
-    startDate:{
-        type:Date,
-    },
-    endDate:{
-        type:Date,
-    }
-},{ _id : false })
+  value: {
+    type: Number,
+    min: 0.01,
+    max: 100
+  },
+  startDate: {
+    type: Date,
+  },
+  endDate: {
+    type: Date,
+  }
+}, { _id: false })
 const productDetailSchema = new Schema<ProductDetailsType>({
-    field:{
-      type:String,
-      required:true
-    },
-    value:{
-      type:String,
-      required:true
-    }
-},{ _id : false })
-const attributeSchema = new Schema<AttributeType>({
-    variant:[]
-    // other fields are dynamically added
-}, { _id : false })
-const productSchema = new Schema<ProductType>({
-  title: {
-    required: true,
+  field: {
     type: String,
-    unique:true
-  },
-  price:{
-    required: true,
-    type:Number
-  },
-  description:{
-    required: true,
-    type:String
-  },
-  discount:discountSchema,
-  attribute:attributeSchema,
-  coverImage:{
-    type:String,
     required: true
   },
-  images:{
-    type:[String],
+  value: {
+    type: String,
+    required: true
+  }
+}, { _id: false })
+const attributeSchema = new Schema<AttributeType>({
+  variant: []
+  // other fields are dynamically added
+}, { _id: false })
+const productSchema = new Schema<ProductType>({
+  title: {
+    required: [true, "Product can't be anonymous"],
+    type: String,
+    unique: true,
+    trim: true
+  },
+  tags: [String],
+  price: {
+    required: true,
+    type: Number,
+    min: 0.01
+  },
+  description: {
+    required: true,
+    type: String,
+    trim: true
+  },
+  discount: discountSchema,
+  attribute: attributeSchema,
+  coverImage: {
+    type: String,
+    required: true
+  },
+  images: {
+    type: [String],
     // required: true
   },
-  productDetails:[productDetailSchema],// other fields are dynamically added
-  artists:{
-    type:[String],
+  productDetails: [productDetailSchema], // other fields are dynamically added
+  artists: {
+    type: [String],
   },
-  songs:{
-    type:[String]
+  songs: {
+    type: [String]
   },
-  productType:{
-    type:String,
-    enum:["release","goods"],
-    default:"goods"
-  }
-},{timestamps:true},);
+  productType: {
+    type: String,
+    enum: ["release", "goods"],
+    default: "goods",
+  },
+  created_at: Number
+}, { timestamps: true },);
 
 const Product: Model<ProductType> = mongoose.model("Product", productSchema);
 export default Product;
