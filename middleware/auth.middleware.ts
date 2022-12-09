4e205e6645589cea5ec3c0ccca4abe3ea2b50f40
Prashantt423 <84055497+Prashantt423@users.noreply.github.com>
2022-12-09 23:49:59 +0530

    orders,users,admin

--- middleware/auth.middleware.ts
+++ middleware/auth.middleware.ts
@@ -51,6 +51,57 @@ class AuthMiddleWare {
       });
     }
   };
+  public static IsAdmin: RequestHandler = async (req, res, next) => {
+    try {
+      // 1) Checking if the token is present insdie the header
+      let token = "";
+      if (
+        req.headers.authorization &&
+        req.headers.authorization.startsWith("Bearer")
+      ) {
+        token = req.headers.authorization.split(" ")[1];
+      }
+      if (!token) {
+        throw new Error("You should login first");
+      }
+      //2) Validating the token
+      console.log("*************************" + token);
+      const decoded = decode(token, process.env.JWT_SECRET, false);
+      console.log(decoded);
+      if (!decoded) {
+        throw new Error("The token is not vaild");
+      }
+      //3) Checking if the user exsists
+
+      const currentUser = await UserRepository.getUser(decoded.id);
+      if (!currentUser.success) {
+        throw new Error("User does not exist");
+      }
+
+      if (
+        UserUsecase.compareTimeStamps(
+          decoded.iat,
+          currentUser.data.passwordChangedAt
+        )
+      ) {
+        throw new Error("Password was recently changed, please login again");
+      }
+      req.user = currentUser.data;
+      if (currentUser.data.role != "ADMIN") {
+        res.status(401).json({
+          status: "failed",
+          message: "Invalid token!",
+        });
+      }
+      next();
+    } catch (err) {
+      console.log(err);
+      res.status(401).json({
+        status: "failed",
+        message: err,
+      });
+    }
+  };
 }
 
 export default AuthMiddleWare;
